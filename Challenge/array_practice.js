ffcec6442c874546b872a4c294bcfaf079676702
vranila <acroiris123@gmail.com>
2026-02-07 16:32:42 +0530

    challenge

---
+++ Challenge/array_practice.js
@@ -0,0 +1,15 @@
+/* 
+-------- PRACTICE CHALLENGE ----------
+Make a file array_practice.js and try these:
+Create an array of 5 city names.
+Add a new city to the end and start.
+Remove the first city.
+Print the array length.
+
+Use a for...of loop to print each city’s name in uppercase. 
+first try your self, then read the code below
+
+*/
+
+let city=["Mumabi","Kannur","Bangalore","Delhi","Pune"];
+city.push("Kollam");
