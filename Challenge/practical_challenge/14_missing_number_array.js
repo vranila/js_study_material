f8d8460786f0bf514e427d091021a17ddb3c842e
vranila <acroiris123@gmail.com>
2026-02-14 18:58:43 +0530

    updation

--- Challenge/practical_challenge/14_missing_number_array.js
+++ Challenge/practical_challenge/14_missing_number_array.js
@@ -1,1 +1,19 @@
-let arr = [1, 2, 3, 5];
+let arr = [1, 2, 4, 5,7,9];
+let n = 10;
+for(let i=0; i<n ;i++)
+{
+    let found=false;
+    for(let j=0 ; j < arr.length;j++)
+    { 
+        if(arr[j]== i)
+        {
+            found =true;
+            break;
+        }
+
+    }
+
+     if (!found) {
+        console.log("Missing Number =", i);
+    }
+}
---
+++ Challenge/practical_challenge/15_frequency_of_word.js
@@ -0,0 +1,15 @@
+let str = "programming";
+
+let frequency = {};
+
+for (let i = 0; i < str.length; i++) {
+    let ch = str[i];
+
+    if (frequency[ch]) {
+        frequency[ch]++;
+    } else {
+        frequency[ch] = 1;
+    }
+}
+
+console.log(frequency);
